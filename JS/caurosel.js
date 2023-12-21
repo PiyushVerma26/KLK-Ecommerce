2f2933882fc1b6443fb892abb4d8256bd4dede5d
PiyushVerma26 <piyushv528@gmail.com>
2023-12-21 14:43:00 +0530

    Making it Compatible for All types of Browser

--- JS/caurosel.js
+++ JS/caurosel.js
@@ -2,20 +2,20 @@ const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
 const caurosel = [
   {
-    title: "Your First Choice in Solar",
+    title: "# Your First Choice in Solar",
     description:
       "Discover the brilliance of solar innovation with our cutting-edge solutions. We're committed to providing you with high-performance solar panels and advanced energy storage systems, paving the way for a sustainable and resilient energy future.",
     src: "panel3.jpg",
   },
   {
-    title: "Efficient. Affordable. Sustainable Energy Solutions",
+    title: "# Efficient. Affordable. Sustainable Energy Solutions",
     description:
       "Experience the revolution in solar energy.Our company is dedicated to delivering state- of - the - art solar solutions that not only reduce your carbon footprint but also offer significant savings on your energy bills.Embrace the power of the sun with us!.",
     src: "panel2.jpg",
   },
 
   {
-    title: "Leading Solar Solutions Provider",
+    title: "# Leading Solar Solutions Provider",
     description:
       "At the forefront of solar technology, we bring you efficient and affordable energy solutions. From residential rooftops to large-scale commercial projects, our commitment to sustainability drives us to provide you with the best in solar innovation.",
 
@@ -44,7 +44,7 @@ function setCaurosel() {
             <img src="../images/caurosel/${items.src}" id="imgCa">
         </div>
         <div class="contentCau">
-            <h1># ${items.title}</h1>
+            <h1>${items.title}</h1>
             <p>${items.description}</p>
             <a href="contact.html" id="connect">Connect With Us</a>
         </div>
